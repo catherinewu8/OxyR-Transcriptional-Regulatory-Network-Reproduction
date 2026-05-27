8428438fba38da99573102166c223772fdcd5f42
Catherine Wu <catherinewu2000@gmail.com>
2026-05-27 13:18:57 -0400

    Upload peak calling scripts for different analysis variations
    The original peak calling script was Ext400_peakCalling.sh where the --extsize in the Peak Calling method was set to 400. This was changed to 80 and 35 to see if the peaks were more resolved. NoNoModel_peakCalling.sh was a test with removing --nomodel that resulted in failure to build model and pair peaks.

---
+++ scripts/Ext80_peakCalling.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --chdir=/home/c/catherinewu/MidtermProject/
+#SBATCH --job-name=CHIPseqPeakCalling
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=10000
+#SBATCH -t 00:15:00
+#SBATCH -o Ext80/run.out
+#SBATCH -e Ext80/run.err
+#SBATCH --mail-user=catherinewu@usf.edu
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+module purge
+module add apps/miniconda/3.6.1-intel
+unset PYTHONPATH
+
+/shares/phc7736.001s26.cw/tools/macs2_env/bin/macs2 callpeak -t SRR1796591.bam -n MACSpeaks_OxyR -q 0.05 -g 4639675 --keep-dup 1 --nomodel --extsize 80 --outdir ./Ext80
+/shares/phc7736.001s26.cw/tools/macs2_env/bin/macs2 callpeak -t SRR1796593.bam -n MACSpeaks_SoxR -q 0.05 -g 4639675 --keep-dup 1 --nomodel --extsize 80 --outdir ./Ext80
+/shares/phc7736.001s26.cw/tools/macs2_env/bin/macs2 callpeak -t SRR1796595.bam -n MACSpeaks_SoxS -q 0.05 -g 4639675 --keep-dup 1 --nomodel --extsize 80 --outdir ./Ext80
