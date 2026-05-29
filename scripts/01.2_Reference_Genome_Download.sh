ec5dc0dca11e40ab8a644d471814da03e414552b
Catherine Wu <catherinewu2000@gmail.com>
2026-05-29 13:37:45 -0400

    Upload reference genome download script
    Upload script for E. coli reference genome NC_00913.3 download. Includes FNA, GFF, and GTF files.

---
+++ scripts/01.2_Reference_Genome_Download.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --chdir=/home/c/catherinewu/MidtermProject/
+#SBATCH --job-name=refGenomeDownload
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=15000
+#SBATCH -t 00:15:00
+#SBATCH -o run.out
+#SBATCH -e run.err
+#SBATCH --mail-user=catherinewu@usf.edu
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
+
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.gff.gz
+
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.gtf.gz
