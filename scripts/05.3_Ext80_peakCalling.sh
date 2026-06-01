71215497644cc35e5c11a64b3b263eb5dc472f4c
Catherine Wu <catherinewu2000@gmail.com>
2026-06-01 13:11:16 -0400

    Adjust peak calling with ext size 80
    Adjusted ext size based on the predicted fragment length of the transcription factor in error files.

--- scripts/05.3_Ext80_peakCalling.sh
+++ scripts/05.3_Ext80_peakCalling.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --chdir=/home/c/catherinewu/MidtermProject/
-#SBATCH --job-name=CHIPseqPeakCalling
+#SBATCH --job-name=ChIPseqPeakCalling
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=10000
@@ -15,8 +15,7 @@
 
 module purge
 module add apps/miniconda/3.6.1-intel
+
 unset PYTHONPATH
 
 /shares/phc7736.001s26.cw/tools/macs2_env/bin/macs2 callpeak -t SRR1796591.bam -n MACSpeaks_OxyR -q 0.05 -g 4639675 --keep-dup 1 --nomodel --extsize 80 --outdir ./Ext80
-/shares/phc7736.001s26.cw/tools/macs2_env/bin/macs2 callpeak -t SRR1796593.bam -n MACSpeaks_SoxR -q 0.05 -g 4639675 --keep-dup 1 --nomodel --extsize 80 --outdir ./Ext80
-/shares/phc7736.001s26.cw/tools/macs2_env/bin/macs2 callpeak -t SRR1796595.bam -n MACSpeaks_SoxS -q 0.05 -g 4639675 --keep-dup 1 --nomodel --extsize 80 --outdir ./Ext80
