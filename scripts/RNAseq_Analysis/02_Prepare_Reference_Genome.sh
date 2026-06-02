da82f6746ffd2dbc658040a53d67d9a3b5047b10
Catherine Wu <catherinewu2000@gmail.com>
2026-06-02 13:59:19 -0400

    Create 02_Prepare_Reference_Genome.sh
    Prepare reference genome using bowtie instead of bowtie2 for a closer reproduction of the original paper (Seo, et. al.).

---
+++ scripts/RNAseq_Analysis/02_Prepare_Reference_Genome.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Use the same reference genome as in the ChIPseq analysis
+
+bowtie-build './GCF_000005845.2_ASM584v2_genomic.fna.gz' './GCF_000005845.2_ASM584v2_refgenomeindex_bow1'
+
