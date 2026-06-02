245ee41662d562f34504c42869defe42b4331f79
Catherine Wu <catherinewu2000@gmail.com>
2026-06-02 13:45:00 -0400

    Create 01_OxyR_RNAseq_Download_Script.sh
    Download script for Wild-Type and OxyR RNAseq data. Library is paired-end.

---
+++ scripts/RNAseq_Analysis/01_OxyR_RNAseq_Download_Script.sh
@@ -0,0 +1,11 @@
+# Download RNAseq data from ENA
+
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR179/000/SRR1796600/SRR1796600_1.fastq.gz
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR179/009/SRR1796599/SRR1796599_1.fastq.gz
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR179/001/SRR1796601/SRR1796601_1.fastq.gz
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR179/008/SRR1796598/SRR1796598_2.fastq.gz
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR179/009/SRR1796599/SRR1796599_2.fastq.gz
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR179/008/SRR1796598/SRR1796598_1.fastq.gz
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR179/000/SRR1796600/SRR1796600_2.fastq.gz
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR179/001/SRR1796601/SRR1796601_2.fastq.gz
+
